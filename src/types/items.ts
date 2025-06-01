3dd39501ff337460d64488ace8086a1431daf80a
Roman A <121314722+GameRoMan@users.noreply.github.com>
2025-06-01 13:18:42 +0100

    Publish package to npm

--- src/types/items.ts
+++ src/types/items.ts
@@ -1,13 +1,11 @@
 import { z } from 'zod';
 
-// Element
 export const ElementSchema = z.object({
 	id: z.string(),
 	emoji: z.string()
 });
 export type ElementType = z.infer<typeof ElementSchema>;
 
-// ItemData
 export const ItemDataSchema = z.object({
 	id: z.number(),
 	text: z.string(),
@@ -18,32 +16,27 @@ export const ItemDataSchema = z.object({
 });
 export type ItemDataType = z.infer<typeof ItemDataSchema>;
 
-// Recipe = tuple[Element, Element]
 export const RecipeSchema = z.tuple([ElementSchema, ElementSchema]);
 export type RecipeType = z.infer<typeof RecipeSchema>;
 
-// RecipesData
 export const RecipesDataSchema = z.object({
 	total: z.number(),
 	recipes: z.array(RecipeSchema)
 });
 export type RecipesDataType = z.infer<typeof RecipesDataSchema>;
 
-// Use
 export const UseSchema = z.object({
 	pair: ElementSchema,
 	result: ElementSchema
 });
 export type UseType = z.infer<typeof UseSchema>;
 
-// UsesData
 export const UsesDataSchema = z.object({
 	total: z.number(),
 	recipes: z.array(RecipeSchema)
 });
 export type UsesDataType = z.infer<typeof UsesDataSchema>;
 
-// Step
 export const StepSchema = z.object({
 	a: ElementSchema,
 	b: ElementSchema,
@@ -51,11 +44,9 @@ export const StepSchema = z.object({
 });
 export type StepType = z.infer<typeof StepSchema>;
 
-// Lineage = list[Step]
 export const LineageSchema = z.array(StepSchema);
 export type LineageType = z.infer<typeof LineageSchema>;
 
-// LineageData
 export const LineageDataSchema = z.object({
 	steps: LineageSchema,
 	missing: z.record(z.union([z.string(), z.literal('loop')]))
